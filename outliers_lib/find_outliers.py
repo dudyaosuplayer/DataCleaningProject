3424b9e827d159dd0a28825bd36317eedc7ce746
Pavlov <alpavlov@neoflex.ru>
2024-11-03 13:55:50 +0400

    Revert "second commit"
    This reverts commit d3dbc995fb70cf67eb2ec3a1e0a77075ce14cc71.

--- outliers_lib/find_outliers.py
+++ outliers_lib/find_outliers.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
 
-
 def find_outliers_iqr(data, feature, left=1.5, right=1.5, log_scale=False):
     """
-    Находит выбросы в данных, используя метод межквартильного размаха.
+    Находит выбросы в данных, используя метод межквартильного размаха. 
     Классический метод модифицирован путем добавления:
     * возможности логарифмирования распредления
     * ручного управления количеством межквартильных размахов в обе стороны распределения
@@ -20,24 +19,20 @@ def find_outliers_iqr(data, feature, left=1.5, right=1.5, log_scale=False):
         pandas.DataFrame: очищенные данные, из которых исключены выбросы
     """
     if log_scale:
-        x = np.log(data[feature] + 1)
+        x = np.log(data[feature]+1)
     else:
-        x = data[feature]
-    quartile_1, quartile_3 = (
-        x.quantile(0.25),
-        x.quantile(0.75),
-    )
+        x= data[feature]
+    quartile_1, quartile_3 = x.quantile(0.25), x.quantile(0.75),
     iqr = quartile_3 - quartile_1
     lower_bound = quartile_1 - (iqr * left)
     upper_bound = quartile_3 + (iqr * right)
-    outliers = data[(x < lower_bound) | (x > upper_bound)]
-    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    outliers = data[(x<lower_bound) | (x > upper_bound)]
+    cleaned = data[(x>lower_bound) & (x < upper_bound)]
     return outliers, cleaned
-
 
 def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     """
-    Находит выбросы в данных, используя метод z-отклонений.
+    Находит выбросы в данных, используя метод z-отклонений. 
     Классический метод модифицирован путем добавления:
     * возможности логарифмирования распредления
     * ручного управления количеством стандартных отклонений в обе стороны распределения
@@ -53,7 +48,7 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
         pandas.DataFrame: очищенные данные, из которых исключены выбросы
     """
     if log_scale:
-        x = np.log(data[feature] + 1)
+        x = np.log(data[feature]+1)
     else:
         x = data[feature]
     mu = x.mean()
@@ -64,6 +59,3 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
 
-
-def new_function(a, b):
-    pass
